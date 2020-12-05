cf8a770d71297bf4703f42f09c9fa5647562542a
Jonathan Yong <yongrenjie@gmail.com>
2020-12-05 14:15:47 +0000

    Refactor Day 4, much better now

--- d4.hs
+++ d4.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.Map.Strict as M
+import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.Text.Read as TR
 import qualified Data.Text.IO as TIO
-import Data.Maybe
-import Data.Monoid
 import Paths_aoc20_hs
 
 main :: IO ()
@@ -12,97 +12,93 @@ main = do
     fname <- getDataFileName "d4.txt"
     input <- TIO.readFile fname
     putStr $ "Part 1: "
-    print $ length . filter isJust . findValidPassports False $ input
+    print $ length . filter isJust . getPassports False $ input
     putStr $ "Part 2: "
-    print $ length . filter isJust . findValidPassports True $ input
+    print $ length . filter isJust . getPassports True $ input
 
-data PassportField = BirthYear T.Text
-                   | IssueYear T.Text
-                   | ExpirationYear T.Text
-                   | Height T.Text
-                   | HairColor T.Text
-                   | EyeColor T.Text
-                   | PassportID T.Text
-                   | CountryID T.Text
-                   deriving (Eq, Show)
-
-data Passport = Passport { byr :: PassportField
-                         , iyr :: PassportField
-                         , eyr :: PassportField
-                         , hgt :: PassportField
-                         , hcl :: PassportField
-                         , ecl :: PassportField
-                         , pid :: PassportField
-                         , cid :: PassportField
-                         }
+data Passport = Passport { byr :: T.Text , iyr :: T.Text , eyr :: T.Text
+                         , hgt :: T.Text , hcl :: T.Text , ecl :: T.Text
+                         , pid :: T.Text , cid :: T.Text }
               deriving (Show)
 
--- splits input into passport-sized lists. each list entry is a field text.
-getFieldTexts :: T.Text -> [[T.Text]]
-getFieldTexts = map T.words . T.splitOn "\n\n"
+-- Converts the input string into a list of valid passports (Just Passport) or
+-- invalid ones (Nothing).
+getPassports :: Bool -> T.Text -> [Maybe Passport]
+getPassports isPartTwo = map makePassport . map ppTextToMap . getPPTexts
+    where makePassport = if isPartTwo then makePassportTough else makePassportLax
 
--- finds a PassportField inside a list of field texts
-findField :: Bool -> T.Text -> [T.Text] -> Maybe PassportField
-findField isPartTwo desiredFieldName = getFirst . foldMap (First . checkF desiredFieldName)
-    where checkF = if isPartTwo then checkField' else checkField
 
--- scans one field text to see whether it matches the desired field name
--- returns the PassportField if it is
-checkField :: T.Text -> T.Text -> Maybe PassportField
-checkField desiredFieldName field =
-    if fieldName /= desiredFieldName
-       then Nothing
-       else case fieldName of
-                 "byr" -> Just (BirthYear fieldValue)
-                 "iyr" -> Just (IssueYear fieldValue)
-                 "eyr" -> Just (ExpirationYear fieldValue)
-                 "hgt" -> Just (Height fieldValue)
-                 "hcl" -> Just (HairColor fieldValue)
-                 "ecl" -> Just (EyeColor fieldValue)
-                 "pid" -> Just (PassportID fieldValue)
-                 "cid" -> Just (CountryID fieldValue)
-                 _     -> error "invalid field requested"
-    where (fieldName:fieldValue:_) = T.split (==':') field
+{- For Part 1 of the question -}
 
--- checkField but with additional validation for part 2
-checkField' :: T.Text -> T.Text -> Maybe PassportField
-checkField' desiredFieldName field =
-    if fieldName /= desiredFieldName
-       then Nothing
-       else case fieldName of
-                 "byr" -> BirthYear <$> validateBounds 1920 2002 fieldValue
-                 "iyr" -> IssueYear <$> validateBounds 2010 2020 fieldValue
-                 "eyr" -> ExpirationYear <$> validateBounds 2020 2030 fieldValue
-                 "hgt" -> Height <$> validateHeight fieldValue
-                 "hcl" -> HairColor <$> validateHCL fieldValue
-                 "ecl" -> EyeColor <$> validateECL fieldValue
-                 "pid" -> PassportID <$> validatePID fieldValue
-                 "cid" -> Just (CountryID fieldValue)
-                 _     -> error "invalid field requested"
-    where (fieldName:fieldValue:_) = T.split (==':') field
+-- Splits input into passport-sized strings
+getPPTexts :: T.Text -> [T.Text]
+getPPTexts = T.splitOn "\n\n"
 
--- checks whether a string is between low and high (inclusive)
--- returns Nothing if it's not an integer of if it's out of bounds
--- returns Just the string if it is valid
+-- Parses passport-sized strings into a Map
+ppTextToMap :: T.Text -> M.Map T.Text T.Text
+ppTextToMap = M.fromList . map parseWords . T.words
+    where parseWords :: T.Text -> (T.Text, T.Text)
+          parseWords word = (fieldName, fieldValue)
+              where (fieldName:fieldValue:_) = T.split (==':') word
+
+-- Parses a Map into a Passport, but with no validation except whether the
+-- field is present.
+makePassportLax :: M.Map T.Text T.Text -> Maybe Passport
+makePassportLax m = Passport <$> M.lookup "byr" m
+                             <*> M.lookup "iyr" m
+                             <*> M.lookup "eyr" m
+                             <*> M.lookup "hgt" m
+                             <*> M.lookup "hcl" m
+                             <*> M.lookup "ecl" m
+                             <*> M.lookup "pid" m
+                             <*> Just "AnyCountry" --  M.lookup "cid" m
+
+
+{- For Part 2 of the question -}
+
+-- Parses a Map into a Passport, but depends on whether the field is present
+-- (M.lookup) as well as whether it satisfies certain properties
+makePassportTough :: M.Map T.Text T.Text -> Maybe Passport
+makePassportTough m = Passport <$> (M.lookup "byr" m >>= validateBounds 1920 2002)
+                               <*> (M.lookup "iyr" m >>= validateBounds 2010 2020)
+                               <*> (M.lookup "eyr" m >>= validateBounds 2020 2030)
+                               <*> (M.lookup "hgt" m >>= validateHeight)
+                               <*> (M.lookup "hcl" m >>= validateHCL)
+                               <*> (M.lookup "ecl" m >>= validateECL)
+                               <*> (M.lookup "pid" m >>= validatePID)
+                               <*> Just "AnyCountry" --  M.lookup "cid" m
+
+-- Checks whether a string is an integer between low and high (inclusive).
+-- Returns Nothing if it's not an integer or if it's out of bounds, or Just the
+-- string if it is valid. We can use this function for the byr, iyr, and eyr
+-- fields.
 validateBounds :: Integer -> Integer -> T.Text -> Maybe T.Text
 validateBounds low high text =
     case TR.decimal text of
          Right (a, _) -> if low <= a && a <= high then Just text else Nothing
          Left _ -> Nothing
 
+-- The remaining validation functions are simply the same as that quoted in the
+-- problem specification.
+
 validateHeight :: T.Text -> Maybe T.Text
 validateHeight text =
     case TR.decimal text of
-         Right (a, "cm") -> if 150 <= a && a <= 193 then Just text else Nothing
-         Right (a, "in") -> if 59 <= a && a <= 76 then Just text else Nothing
+         -- the explicit type just silences a -Wtype-defaults ghc warning. not
+         -- actually critical
+         Right (a, "cm") -> if (150 :: Integer) <= a && a <= 193
+                               then Just text else Nothing
+         Right (a, "in") -> if 59 <= a && a <= 76
+                               then Just text else Nothing
          _ -> Nothing
 
 validateHCL :: T.Text -> Maybe T.Text
-validateHCL text =
-    case (T.head text, T.length text) of
-         ('#', 7) -> if T.all (`elem` ("ABCDEFabcdef1234567890" :: String)) (T.tail text)
-                        then Just text else Nothing
-         _ -> Nothing
+validateHCL text = if headIsHash && tailLengthIs6 && tailIsHex
+                      then Just text
+                      else Nothing
+    where headIsHash    = T.head text == '#'
+          tailLengthIs6 = T.length text == 7
+          tailIsHex     = T.all (`elem` ("ABCDEFabcdef1234567890" :: String)) (T.tail text)
 
 validateECL :: T.Text -> Maybe T.Text
 validateECL text = if elem text ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
@@ -110,23 +106,6 @@ validateECL text = if elem text ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"
                       else Nothing
 
 validatePID :: T.Text -> Maybe T.Text
-validatePID text =
-    case T.length text of
-         9 -> if T.all (`elem` ("1234567890" :: String)) text then Just text else Nothing
-         _ -> Nothing
-
-
--- parses a line of text into a Passport.
-makePassport :: Bool -> [T.Text] -> Maybe Passport
-makePassport isPartTwo fieldTexts = Passport <$> findField isPartTwo "byr" fieldTexts
-                                             <*> findField isPartTwo "iyr" fieldTexts
-                                             <*> findField isPartTwo "eyr" fieldTexts
-                                             <*> findField isPartTwo "hgt" fieldTexts
-                                             <*> findField isPartTwo "hcl" fieldTexts
-                                             <*> findField isPartTwo "ecl" fieldTexts
-                                             <*> findField isPartTwo "pid" fieldTexts
-                                             <*> Just (CountryID "doesntmatter")
-
--- parses the input using all the previous functions.
-findValidPassports :: Bool -> T.Text -> [Maybe Passport]
-findValidPassports isPartTwo input = map (makePassport isPartTwo) . getFieldTexts $ input
+validatePID text = if lengthIs9 && isNumeric then Just text else Nothing
+    where lengthIs9 = T.length text == 9
+          isNumeric = T.all (`elem` ("1234567890" :: String)) text
