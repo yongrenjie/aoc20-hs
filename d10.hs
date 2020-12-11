063a9fa016adb7e6304ffa235f7e4b05ad95f745
Jonathan Yong <yongrenjie@gmail.com>
2020-12-11 03:19:47 +0000

    Incomplete Day 10

---
+++ d10.hs
@@ -0,0 +1,34 @@
+import Data.List
+import Paths_aoc20_hs
+
+main :: IO ()
+main = do
+    fname <- getDataFileName "d10.txt"
+    input <- readFile fname
+    let adaptors = sort . map (read :: String -> Int) . lines $ input
+    let (diff1, _, diff3) = getJoltDiffs (0:adaptors)
+    putStrLn $ "Part 1: " ++ show (diff1 * diff3)
+    putStrLn $ "Part 2: " ++ show (countValidCombos 0 adaptors)
+
+-- assumes input is sorted, also adds the 3-jolt difference for the device
+-- itself.
+getJoltDiffs :: [Int] -> (Int, Int, Int)
+getJoltDiffs [] = (0, 0, 1)
+getJoltDiffs (_:[]) = (0, 0, 1)
+getJoltDiffs (x:y:ys) = (p + dp, q + dq, r + dr)
+    where (p, q, r) = getJoltDiffs (y:ys)
+          (dp, dq, dr) = case y - x of
+                              1 -> (1, 0, 0)
+                              2 -> (0, 1, 0)
+                              3 -> (0, 0, 1)
+                              _ -> error "invalid input"
+
+-- Brute force. Too slow!!
+countValidCombos :: Int -> [Int] -> Int
+countValidCombos _ [] = 1
+countValidCombos _ (_:[]) = 1
+countValidCombos p (_:r:[]) = if r - p <= 3 then 2 else 1
+countValidCombos p (q:r:s:x) = cq + cr + cs
+    where cq = if q - p <= 3 then countValidCombos q (r:s:x) else 0
+          cr = if r - p <= 3 then countValidCombos r (s:x) else 0
+          cs = if s - p <= 3 then countValidCombos s (x) else 0
