1d7854b9bf9c55468a265468b34134ef6aea1bbb
Jonathan Yong <yongrenjie@gmail.com>
2020-12-24 00:10:04 +0000

    Change C code to use 1-indexing

--- d23b.c
+++ d23b.c
@@ -2,28 +2,41 @@
 #include <stdlib.h>
 #include "d23b.h"
 
+/* int main (void) */
+/* { */
+/*     long int ans = aoc_d23_part2(); */
+/*     printf("%ld\n", ans); */
+/* } */
+
 long int aoc_d23_part2(void)
 {
     int first_input[] = {6, 2, 4, 3, 9, 7, 1, 5, 8};
     return run_game(first_input, 9, 1000000, 10000000);
+
+    /* Part 1 */
+    /* int first_input[] = {3, 8, 9, 1, 2, 5, 4, 6, 7}; */
+    /* return run_game(first_input, 9, 9, 10); */
 }
 
 long int run_game(int *first_input, int first_input_len, int full_len, int nrounds) {
     // Initialise the game array
-    int *game = malloc(sizeof(int) * full_len);
-    // Fill in the provided values
+    int *game = malloc(sizeof(int) * full_len + 1);
+    // Fill in the provided values. game[n] will contain the number that is after n.
+    game[0] = 0;  // Helps me avoid off-by-one errors.
     for (int i = 0; i < first_input_len - 1; i++) {
-        game[first_input[i] - 1] = first_input[i + 1];
+        game[first_input[i]] = first_input[i + 1];
     }
+    // Pad with extra numbers if necessary.
     if (full_len > first_input_len) {
-        game[first_input[first_input_len - 1] - 1] = first_input_len + 1;
-        for (int i = first_input_len; i < full_len - 1; i++) {
-            game[i] = i + 2;
+        game[first_input[first_input_len - 1]] = first_input_len + 1;
+        for (int i = first_input_len + 1; i < full_len; i++) {
+            game[i] = i + 1;
         }
-        game[full_len - 1] = first_input[0];
+        game[full_len] = first_input[0];
     }
+    // Otherwise, just close the loop.
     else {
-        game[first_input[first_input_len - 1] - 1] = first_input[0];
+        game[first_input[first_input_len - 1]] = first_input[0];
     }
     int current = first_input[0];
 
@@ -39,27 +52,27 @@ long int run_game(int *first_input, int first_input_len, int full_len, int nroun
 int *update(int *game, int *current, int length)
 {
     // Pick out the three cups
-    int a = game[*current - 1];
-    int b = game[a - 1];
-    int c = game[b - 1];
-    int d = game[c - 1];
-    game[*current - 1] = d;
+    int a = game[*current];
+    int b = game[a];
+    int c = game[b];
+    int d = game[c];
+    game[*current] = d;
 
-    // Figure out what the destination cup is. It needs to be smaller than a,
-    // *unless* there is no such cup in which case it needs to be the largest
-    // available cup.
+    // Figure out what the destination cup is. It needs to be smaller than the
+    // current value, *unless* there is no such cup in which case it needs to
+    // be the largest available cup.
     int destination = *current - 1;
     for (;;) {
         if (destination < 1) destination = length;
         if (destination == a || destination == b || destination == c) {
-            destination--;  // not found
+            destination--;  // was picked up already
         }
         else break;
     }
 
     // Insert the three cups back in
-    game[c - 1] = game[destination - 1]; // link third cup -> cup after destination
-    game[destination - 1] = a;       // link destination -> first cup
+    game[c] = game[destination]; // link third cup -> cup after destination
+    game[destination] = a;       // link destination -> first cup
     // Move the current cup to the next
     *current = d;
     
@@ -71,12 +84,12 @@ void print_game(int *game, int *current, int length)
     int a = *current;
     for (int i = 0; i < length; i++) {
         printf("%d ", a);
-        a = game[a - 1];
+        a = game[a];
     }
     printf("\n");
 }
 
 long int count_score(int *game)
 {
-    return game[0] * game[game[0] - 1];
+    return (long int) game[1] * game[game[1]];
 }
